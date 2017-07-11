ff3c4a7d32f6f5f5dd9e58333eb98247ae2f5fce
MarcosFortuna <marcos.fortuna97@gmail.com>
2017-07-11 13:18:05 -0300

    Add files via upload
    Função de abrir e fechar arquivo.

---
+++ ori_arquivos.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void fecharArquivo(File **arquivo){
+      fclose(*arquivo);
+      **arquivo= NULL;
+}
+
+void abrirArquivo(File **arquivo, char modo, const char *nome_arquivo){
+     switch(modo){
+         case'L': {*arquivo =fopen(nome_arquivo,rb); // l para leitura
+                     if(*arquivo){
+                        printf("!ERRO, Arquivo não foi aberto);
+                        exit(-1);
+                     }
+                     break;}
+         case'E': {*arquivo = fopen(nome_arquivo,wb); // somente escrita
+                     if(*arquivo){
+                        printf("!ERRO, Arquivo não foi aberto);
+                        exit(-1); 
+                     }
+                     break;}       
+         case'A': {*arquivo = fopen(nome_arquivo,ab); // leitura e escrita
+                     if(*arquivo){
+                        printf("!ERRO, Arquivo não foi aberto);
+                        exit(-1); 
+                     }
+                     break;}
+         }                  
+}          
